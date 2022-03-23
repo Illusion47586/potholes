a29f8fb4cb4e6cd519102eb3bd721a2e31557ac5
illusion <dhruvtiwari110058@gmail.com>
2022-03-23 11:35:23 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import ImageSelect from "./components/imageSelect/ImageSelect";
 
 import Navbar from "./components/navbar/Navbar";
+import Maps from "./components/maps/Maps";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <ImageSelect />
+      {/* <ImageSelect /> */}
+      <Maps />
     </div>
   );
 }
