75e9d9da9a90773c88d91cd06c150a33b10e3aa4
illusion <dhruvtiwari110058@gmail.com>
2022-04-16 16:11:18 +0530

    mapbox init

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Navbar from "./components/navbar/Navbar";
 import ImageSelect from "./components/imageSelect/ImageSelect";
 import ContactUs from "./components/contactUs/ContactUs";
 import "./App.css";
-import Maps from "./components/maps/Maps";
+import MapBox from "./components/maps/Mapbox";
 import { ToastContainer } from "react-toastify";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 import "react-toastify/dist/ReactToastify.css";
 import OpenModal from "./components/openModal/OpenModal";
@@ -13,7 +14,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Maps />} />
+        <Route path="/" exact element={<MapBox />} />
         <Route path="/report" exact element={<ImageSelect />} />
         <Route path="/us" exact element={<ContactUs />} />
       </Routes>
---
+++ src/components/maps/Mapbox.js
@@ -0,0 +1,145 @@
+import React, { useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import styles from "./maps.module.scss";
+import axios from "axios";
+
+import Map, { Marker } from "react-map-gl";
+
+function getDistanceFromLatLonInM(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d * 1000;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+const MapBox = () => {
+  const map = useRef(null);
+  const [center, setCenter] = React.useState({ lat: 0, lng: 0 });
+
+  const [potholes, setPotholes] = React.useState([]);
+
+  function success(pos) {
+    const crd = pos.coords;
+
+    if (
+      center === null ||
+      center.lat !== crd.latitude ||
+      center.lng !== crd.longitude
+    ) {
+      console.log("Updating");
+      setCenter({ lat: crd.latitude, lng: crd.longitude });
+    }
+  }
+
+  const getPotholes = async () => {
+    const potHoles = await axios.get(
+      `https://potholesserver.herokuapp.com/potholeByDistance?lat=${center.lat}&long=${center.lng}`
+    );
+    // const potHoles = await axios.get(
+    //   "https://potholesserver.herokuapp.com/potholeByDistance?lat=28.6488951&long=77.040059"
+    // );
+    const p = [];
+    potHoles.data.forEach((v, i) => {
+      if (
+        getDistanceFromLatLonInM(
+          v.location.coordinates[1],
+          v.location.coordinates[0],
+          center.lat,
+          center.lng
+        ) <= 20
+      ) {
+        toast("Caution, pothole nearby.", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+
+      p.push(v.location.coordinates);
+    });
+    console.log(p);
+    setPotholes(p);
+  };
+
+  useEffect(() => {
+    console.log("Center: ", center);
+    if (center != null && map != null) {
+      const b = 0.00021;
+      const bounds = {
+        north: -b + center.lat,
+        south: b + center.lat,
+        west: -b + center.lng,
+        east: b + center.lng,
+      };
+
+      map.current?.fitBounds(
+        [
+          [bounds.east, bounds.south],
+          [bounds.west, bounds.north],
+        ],
+        { padding: 10, duration: 1000 }
+      );
+      getPotholes();
+      //   setMap(map);
+    }
+  }, [center]);
+  function error(err) {
+    // alert("ERROR(" + err.code + "): " + err.message);
+    console.warn("ERROR(" + err.code + "): " + err.message);
+  }
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(success, error, options);
+  });
+
+  return (
+    <div className={styles.mapContainer}>
+      <Map
+        mapboxAccessToken="pk.eyJ1IjoiZGhydXY0NzU4NiIsImEiOiJjbDIxbjFxdWQwMm90M2Vvb2s1cjg0YzUwIn0.dF0jG5YjBNyDYrWsa9j3HQ"
+        ref={map}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        initialViewState={{
+          latitude: center.lat,
+          longitude: center.lng,
+          zoom: 9,
+        }}
+        testMode
+      >
+        <Marker longitude={center.lng} latitude={center.lat}>
+          <img src="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" />
+        </Marker>
+        {potholes.map((p) => (
+          <Marker longitude={p[0]} latitude={p[1]}>
+            <img src="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" />
+          </Marker>
+        ))}
+      </Map>
+    </div>
+  );
+};
+
+export default React.memo(MapBox);
