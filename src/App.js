11db72cb7d28a4bc35b2533caf820e1287c10b0c
Naman Singh <snaman431@gmail.com>
2022-04-21 17:16:13 +0530

    embedded figma

--- src/App.js
+++ src/App.js
@@ -16,6 +16,19 @@ function App() {
         <Route path="/" exact element={<MapBox />} />
         <Route path="/report" exact element={<ImageSelect />} />
         <Route path="/us" exact element={<ContactUs />} />
+        <Route
+          path="/reported"
+          exact
+          element={
+            <iframe
+              width="100%"
+              height="850"
+              title="reported"
+              src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FS6MjbQEtiUxawD8C1H4DX1%2FSIH%3Fnode-id%3D188%253A303%26scaling%3Dmin-zoom%26page-id%3D0%253A1%26starting-point-node-id%3D180%253A315"
+              allowFullScreen
+            ></iframe>
+          }
+        />
       </Routes>
       <ToastContainer />
     </div>
